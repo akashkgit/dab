5975ac1720d9978091fb8610a40387863f00ff6f
Srinidhi Sasidharan <srinidhisasidharan99@gmail.com>
2024-05-05 19:31:40 -0400

    levelOneView

---
+++ up/Controllers/LevelOneViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LevelOneViewController.swift
+//  levels
+//
+//  Created by Srinidhi Sasidharan on 5/5/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class LevelOneViewController: UIViewController {
+    
+    override func loadView() {
+        self.view = SKView()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.modalPresentationStyle = .fullScreen
+    }
+    func levelOneScreen(){
+        if let view = self.view as! SKView? {
+            if let scene = SKScene(fileNamed: "GameScene") {
+                scene.scaleMode = .fill
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
